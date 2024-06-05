76babaa4af0c6882d3edfe7002b17ce79bee5ae3
ecorey <bengstrom32@gmail.com>
2024-06-05 10:55:38 -0400

    first commit

--- phaser-webpack-boilerplate-main/phaser-webpack-boilerplate-main/src/index.js
+++ phaser-webpack-boilerplate-main/phaser-webpack-boilerplate-main/src/index.js
@@ -25,11 +25,24 @@ function preload() {
   
   this.load.image('sky', 'assets/sky.png');
 
+  this.load.image('bird', 'assets/bird.png');
+
+  
+
 }
 
 
+let bird = null;
+
 function create() {
-  this.add.image(0, 0, 'sky');
+
+  this.add.image(0, 0, 'sky').setOrigin(0);
+
+  bird = this.physics.add.sprite(config.width / 16, config.height / 2, 'bird').setOrigin(0);
+  
+  // pulls bird down and off screen
+  bird.body.gravity.y = 200;
+  
 }
 
 

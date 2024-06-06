3a1a100b6b2ac8f013695ce286e5094b91a3c5ef
ecorey <bengstrom32@gmail.com>
2024-06-06 13:47:46 -0400

    commit

--- phaser-webpack-boilerplate-main/phaser-webpack-boilerplate-main/src/scenes/PlayScene.js
+++ phaser-webpack-boilerplate-main/phaser-webpack-boilerplate-main/src/scenes/PlayScene.js
@@ -1,7 +1,12 @@
 import Phaser from 'phaser';
 
 
+const PIPES_TO_RENDER = 4;
+
+
 class PlayScene extends Phaser.Scene {
+
+
 
 
     constructor(config) {
@@ -11,6 +16,12 @@ class PlayScene extends Phaser.Scene {
         this.config = config;
 
         this.bird = null;
+        this.pipes = null;
+
+        this.pipeHorizontalDistance = 0;
+        this.pipeVerticalDistanceRange = [150, 250];
+        this.pipeHorizontalDistanceRange = [500, 600];
+        this.flapVelocity = 150;
 
     }
 
@@ -22,6 +33,8 @@ class PlayScene extends Phaser.Scene {
         this.load.image('sky', 'assets/sky.png');
 
         this.load.image('bird', 'assets/bird.png');
+
+        this.load.image('pipe', 'assets/pipe.png');
 
     }
 
@@ -36,6 +49,37 @@ class PlayScene extends Phaser.Scene {
         
         this.bird.body.gravity.y = 400;
 
+    
+        this.pipes = this.physics.add.group();
+        
+        
+        // bird.body.velocity.x = VELOCITY;
+
+        for(let i = 0; i < PIPES_TO_RENDER; i++) {
+
+            // creates and adds to the group
+            const upperPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0,1);
+            const lowerPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0,0);
+
+            
+
+            this.placePipe(upperPipe, lowerPipe)
+
+        }
+
+        
+        this.pipes.setVelocityX(-200);
+        
+
+
+        // pressing mouse button
+        this.input.on('pointerdown', this.flap, this);
+
+
+        // pressing space bar
+        this.input.keyboard.on('keydown_SPACE', this.flap, this);
+        
+
     }
 
 
@@ -43,8 +87,118 @@ class PlayScene extends Phaser.Scene {
 
     update() {
 
+        if( this.bird.y > this.config.height || this.bird.y < -this.bird.height) {
+            this.restartBirdPosition();
+          }
+        
+        
+          // recycles pipes in the group
+          this.recyclePipes();
+
+
     }
 
+
+
+    placePipe(uPipe, lPipe) {
+
+        // pipeHorizontalDistance += 400;
+        // pipeHorizontalDistance = getRightMostPipe();
+    
+        const rightMostX = this.getRightMostPipe();
+    
+        const pipeVerticalDistance = Phaser.Math.Between(...this.pipeVerticalDistanceRange);
+        const pipeVerticalPosition = Phaser.Math.Between(0 + 20, this.config.height - 20 -pipeVerticalDistance);
+        
+        const pipeHorizontalDistance = Phaser.Math.Between(...this.pipeHorizontalDistanceRange);
+    
+    
+    
+    
+        // upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0,1);
+        // lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0,0);
+      
+        uPipe.x = rightMostX + pipeHorizontalDistance;
+        uPipe.y = pipeVerticalPosition;
+        
+    
+        lPipe.x = uPipe.x;
+        lPipe.y = uPipe.y + pipeVerticalDistance;
+        
+    
+        
+    }
+
+
+
+    // recycle pipes
+    recyclePipes() {
+
+        const tempPipes = [];
+    
+        this.pipes.getChildren().forEach(pipe => {
+    
+        if(pipe.getBounds().right <= 0){
+            // get upper and lower pipe that are out of bounds
+            tempPipes.push(pipe);
+            if(tempPipes.length == 2) {
+            this.placePipe(...tempPipes);
+            }
+    
+            
+        }
+    
+        })
+    
+    }
+
+
+
+    
+    // get right most pipe
+    getRightMostPipe() {
+
+        let rightMostX = 0;
+    
+        this.pipes.getChildren().forEach(function (pipe){
+        rightMostX = Math.max(pipe.x, rightMostX);
+        })
+    
+    
+        return rightMostX;
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    // to make the bird flap
+    flap() {
+        this.bird.body.velocity.y = -this.flapVelocity;
+    }
+    
+    
+    
+    
+    // restart the bird position to the initial position
+    restartBirdPosition() {
+    
+        this.bird.x = this.config.startPosition.x;
+        this.bird.y = this.config.startPosition.y;
+    
+        this.bird.body.velocity.y = 0;
+        
+    
+    }
+        
+
+
+
+    
 
 
 

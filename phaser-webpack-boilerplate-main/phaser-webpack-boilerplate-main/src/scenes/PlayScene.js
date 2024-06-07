7d5ecdf3131917ffe528b56d0b6090669f106ae6
ecorey <bengstrom32@gmail.com>
2024-06-07 17:37:24 -0400

    commit

--- phaser-webpack-boilerplate-main/phaser-webpack-boilerplate-main/src/scenes/PlayScene.js
+++ phaser-webpack-boilerplate-main/phaser-webpack-boilerplate-main/src/scenes/PlayScene.js
@@ -147,7 +147,10 @@ class PlayScene extends BaseScene {
 
 
     createBird() {
-        this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0);
+        this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird')
+        .setFlipX(true)
+        .setOrigin(0)
+        .setScale(3);
         this.bird.body.gravity.y = 600;
         this.bird.setCollideWorldBounds(true);
     }
